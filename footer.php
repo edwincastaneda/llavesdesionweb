564db8740503a1d71620a8f812e68777e5f3148a
ecastaneda <ecastaneda@prensalibre.com.gt>
2016-09-02 09:11:14 -0600

    primera carga

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/funciones.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,111 @@
+<?php include "header.php"; ?>
+
+<div class="navbar  navbar-fixed-top opaque-navbar">
+  <div class="container transparent">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navMain">
+  <span class="glyphicon glyphicon-chevron-right" style="color:white;"></span>
+
+  </button>
+      <a class="navbar-logo" href="#">
+          <img src="http://fakeimg.pl/150x70/cecece/">
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="navMain">
+      <ul class="nav navbar-nav pull-right">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Services and Pricing</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="container principal">
+
+<!-- BOXES -->
+  <div class="container-boxes">
+<div class="col-xs-2 color_green boxes">
+  <a href="#">
+  <span class="glyphicon glyphicon-headphones" aria-hidden="true"></span>
+<h4>Headphones</h4>
+</a>
+</div>
+
+<div class="col-xs-2 color_orange boxes">
+  <a href="#">
+  <span class="glyphicon glyphicon-volume-down" aria-hidden="true"></span>
+<h4>Volume Down</h4>
+</a>
+</div>
+
+<div class="col-xs-2 color_purple boxes">
+  <a href="#">
+  <span class="glyphicon glyphicon-camera" aria-hidden="true"></span>
+<h4>Camera</h4>
+</a>
+</div>
+
+<div class="col-xs-2 color_red boxes">
+  <a href="#">
+  <span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
+<h4>Map Marker</h4>
+</a>
+</div>
+
+<div class="col-xs-2 color_yellow boxes">
+  <a href="#">
+  <span class="glyphicon glyphicon-fire" aria-hidden="true"></span>
+  <h4>Fire</h4>
+</a>
+</div>
+
+<div class="col-xs-2 color_blue boxes">
+  <a href="#">
+  <span class="glyphicon glyphicon-plane" aria-hidden="true"></span>
+<h4>Plane</h4>
+</a>
+</div>
+
+  </div>
+
+  <!-- BOXES -->
+
+  <div class="jumbotron">
+    <h1>Jumbotron heading</h1>
+    <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+    <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+  </div>
+
+  <div class="row marketing">
+    <div class="col-lg-6">
+      <h4>Subheading</h4>
+      <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+      <h4>Subheading</h4>
+      <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+      <h4>Subheading</h4>
+      <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+    </div>
+
+    <div class="col-lg-6">
+      <h4>Subheading</h4>
+      <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+      <h4>Subheading</h4>
+      <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+      <h4>Subheading</h4>
+      <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+    </div>
+  </div>
+
+  <footer class="footer">
+    <p>&copy; 2016 Company, Inc.</p>
+  </footer>
+
+</div> <!-- /container -->
+
+<?php include "footer.php" ?>
